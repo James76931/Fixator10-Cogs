ffb6857015b7eb707ee473749ea613429b4936d4
James76931 <james76931@me.com>
2021-04-24 18:28:13 +0100

    experiment with new boxes

--- leveler/leveler.py
+++ leveler/leveler.py
@@ -2571,8 +2571,8 @@ class Leveler(commands.Cog):
             info_color = (30, 30, 30, 220)
 
         draw.rectangle([(left_pos - 20, vert_pos + title_height), (right_pos, 156)], fill=info_color)  # title box
-        draw.rectangle([(100, 159), (285, 212)], fill=info_color)  # general content
-        draw.rectangle([(100, 215), (285, 285)], fill=info_color)  # info content
+        draw.rectangle([(100, 159), (285, 200)], fill=info_color)  # general content
+        draw.rectangle([(100, 219), (285, 285)], fill=info_color)  # info content
 
         # stick in credits if needed
         # if bg_url in bg_credits.keys():
