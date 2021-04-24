c64abdda99b8dc2c1d558847745a0418a6e29a0f
James76931 <james76931@me.com>
2021-04-24 17:44:17 +0100

    Try removing credits and global info

--- leveler/leveler.py
+++ leveler/leveler.py
@@ -2718,34 +2718,34 @@ class Leveler(commands.Cog):
             info_text_color,
         )  # Rank
 
-        s_exp_txt = self._truncate_text(f"{await self._find_server_exp(user, server)}", 8)
-        _write_unicode(s_exp_txt, num_local_align, 180, general_info_fnt, general_info_u_fnt, info_text_color)  # Exp
-        credits = await bank.get_balance(user)
-        credit_txt = "£{}".format(credits)
-        draw.text(
-            (num_local_align, 195), self._truncate_text(credit_txt, 18), font=general_info_fnt, fill=info_text_color,
-        )  # Credits
+        #s_exp_txt = self._truncate_text(f"{await self._find_server_exp(user, server)}", 8)
+        #_write_unicode(s_exp_txt, num_local_align, 180, general_info_fnt, general_info_u_fnt, info_text_color)  # Exp
+        #credits = await bank.get_balance(user)
+        #credit_txt = "£{}".format(credits)
+        #draw.text(
+        #    (num_local_align, 195), self._truncate_text(credit_txt, 18), font=general_info_fnt, fill=info_text_color,
+        #)  # Credits
 
         # global stats
-        num_align = 230
-        if "linux" in platform.system().lower():
-            global_symbol = "\U0001F30E "
-            fine_adjust = 1
-        else:
-            global_symbol = "G "
-            fine_adjust = 0
+        #num_align = 230
+        #if "linux" in platform.system().lower():
+        #    global_symbol = "\U0001F30E "
+        #    fine_adjust = 1
+        #else:
+        #    global_symbol = "G "
+        #    fine_adjust = 0
 
-        global_rank = await self._find_global_rank(user)
-        rank_number = global_rank if global_rank else "1000+"
-        rank_txt = global_symbol + self._truncate_text(f"#{rank_number}", 8)
-        exp_txt = self._truncate_text(f"{userinfo['total_exp']}", 8)
-        _write_unicode(
-            rank_txt,
-            num_align - general_info_u_fnt.getsize(global_symbol)[0] + fine_adjust,
-            165,
-            general_info_fnt,
-            general_info_u_fnt,
-            info_text_color,
+        #global_rank = await self._find_global_rank(user)
+        #rank_number = global_rank if global_rank else "1000+"
+        #rank_txt = global_symbol + self._truncate_text(f"#{rank_number}", 8)
+        #exp_txt = self._truncate_text(f"{userinfo['total_exp']}", 8)
+        #_write_unicode(
+        #    rank_txt,
+        #    num_align - general_info_u_fnt.getsize(global_symbol)[0] + fine_adjust,
+        #    165,
+        #    general_info_fnt,
+        #    general_info_u_fnt,
+        #    info_text_color,
         )  # Rank
         _write_unicode(exp_txt, num_align, 180, general_info_fnt, general_info_u_fnt, info_text_color)  # Exp
 
